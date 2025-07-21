4b2f4ec6603a6e8281cb1ef2e7c00c50b852b018
Priyanshu-010 <priyanshurai2772@gmail.com>
2025-07-21 15:28:07 +0530

    Authentication with help of cleark completed

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,19 @@
-
+"use client"
+import { SignedIn, SignedOut, SignInButton } from "@clerk/clerk-react";
+import { SignOutButton } from "@clerk/nextjs";
 
 function page() {
   return (
-    <div>page</div>
-  )
+    <div>
+      Home page
+      <SignedOut>
+        <SignInButton />
+      </SignedOut>
+      <SignedIn>
+        <SignOutButton />
+      </SignedIn>
+    </div>
+  );
 }
 
-export default page
+export default page;
